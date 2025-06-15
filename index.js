c6c0f66dede2e2771cdc51ab0d4f23451e139c03
martinmbuba <martinmbuba4@gmail.com>
2025-06-15 21:29:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Return the first two drivers from an array
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+// Return the last two drivers from an array
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+// Array of the two driver-selection functions
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// Higher-order function that returns a fare-multiplying function
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  };
+}
+
+// Fare-doubling function
+const fareDoubler = createFareMultiplier(2);
+
+// Fare-tripling function
+const fareTripler = createFareMultiplier(3);
+
+// Function that selects different drivers using the provided function
+function selectDifferentDrivers(drivers, driverSelectorFn) {
+  return driverSelectorFn(drivers);
+}
